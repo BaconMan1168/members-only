9f557da8c8c5107dc67093720c6fce114a13407d
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-11-09 15:45:47 +0800

    Added routes for the userRouter

---
+++ routes/userRouter.js
@@ -0,0 +1,15 @@
+const userController = require('../controllers/userController')
+const { Router } = require('express');
+
+const userRouter = Router();
+
+userRouter.get('/sign-up', userController.getSignUpForm);
+userRouter.post('/sign-up', userController.registerUser);
+userRouter.get('/login', userController.getLoginForm);
+userRouter.post('/login', userController.loginUser);
+userRouter.get('/member-form', userController.getMemberForm);
+userRouter.post('/member-form', userController.makeUserMember);
+userRouter.get('/admin-form', userController.getAdminForm);
+userRouter.post('/admin-form', userController.makeUserAdmin);
+
+module.exports = userRouter;
